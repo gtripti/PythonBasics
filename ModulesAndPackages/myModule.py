7a6b5390bffce38c7b00f814d844a1358645d568
gtripti <tripti.gupta97@gmail.com>
2019-08-17 23:14:46 +0530

    module

---
+++ ModulesAndPackages/myModule.py
@@ -0,0 +1,2 @@
+def my_func():
+    print("Hey i am my_module.py")
---
+++ ModulesAndPackages/myProgram.py
@@ -0,0 +1,7 @@
+# from myModule import my_func
+# my_func()
+
+from MyMainPackage import some_main_script
+from MyMainPackage.SubModule import mySubScript
+some_main_script.report_main()
+mySubScript.sub_report()
